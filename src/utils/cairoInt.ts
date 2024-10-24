af633431d561a64a0c4ac7b31ca1f27db7faad8c
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2024-10-24 12:05:00 +0700

    ✨ feat(app): improve cairoint parser

--- src/utils/cairoInt.ts
+++ src/utils/cairoInt.ts
@@ -1,48 +1,48 @@
 import {CairoUint256, CairoUint512, num, type Uint256, type Uint512} from 'starknet'
 
 interface SignedInteger {
-  mag: number | bigint | Uint256 | Uint512
+  mag: number | bigint | `0x${string}` | Uint256 | Uint512
   sign: boolean
 }
 
-function isU256(num: unknown): num is Uint256 {
+function isU256(n: unknown): n is Uint256 {
   return (
-    num !== null &&
-    typeof num === 'object' &&
-    'low' in num &&
-    'high' in num &&
-    ['string', 'number', 'bigint'].includes(typeof num.low) &&
-    ['string', 'number', 'bigint'].includes(typeof num.high)
+    n !== null &&
+    typeof n === 'object' &&
+    'low' in n &&
+    'high' in n &&
+    ['string', 'number', 'bigint'].includes(typeof n.low) &&
+    ['string', 'number', 'bigint'].includes(typeof n.high)
   )
 }
 
-function isU512(num: unknown): num is Uint512 {
+function isU512(n: unknown): n is Uint512 {
   return (
-    num !== null &&
-    typeof num === 'object' &&
-    'limb0' in num &&
-    'limb1' in num &&
-    'limb2' in num &&
-    'limb3' in num &&
-    ['string', 'number', 'bigint'].includes(typeof num.limb0) &&
-    ['string', 'number', 'bigint'].includes(typeof num.limb1) &&
-    ['string', 'number', 'bigint'].includes(typeof num.limb2) &&
-    ['string', 'number', 'bigint'].includes(typeof num.limb3)
+    n !== null &&
+    typeof n === 'object' &&
+    'limb0' in n &&
+    'limb1' in n &&
+    'limb2' in n &&
+    'limb3' in n &&
+    ['string', 'number', 'bigint'].includes(typeof n.limb0) &&
+    ['string', 'number', 'bigint'].includes(typeof n.limb1) &&
+    ['string', 'number', 'bigint'].includes(typeof n.limb2) &&
+    ['string', 'number', 'bigint'].includes(typeof n.limb3)
   )
 }
 
-function isSignedInteger(num: unknown): num is SignedInteger {
+function isSignedInteger(n: unknown): n is SignedInteger {
   return (
-    num !== null &&
-    typeof num === 'object' &&
-    'sign' in num &&
-    typeof num.sign === 'boolean' &&
-    'mag' in num &&
-    (typeof num.mag === 'number' ||
-      typeof num.mag === 'bigint' ||
-      typeof num.mag === 'string' ||
-      isU256(num.mag) ||
-      isU512(num.mag))
+    n !== null &&
+    typeof n === 'object' &&
+    'sign' in n &&
+    typeof n.sign === 'boolean' &&
+    'mag' in n &&
+    (typeof n.mag === 'number' ||
+      typeof n.mag === 'bigint' ||
+      (typeof n.mag === 'string' && num.isHex(n.mag)) ||
+      isU256(n.mag) ||
+      isU512(n.mag))
   )
 }
 
@@ -52,7 +52,8 @@ export function cairoIntToBigInt(
   if (typeof n === 'object') {
     if (isSignedInteger(n)) return cairoIntToBigInt(n.mag) * (n.sign ? -1n : 1n)
     if (isU256(n)) return new CairoUint256(n).toBigInt()
-    return new CairoUint512(n).toBigInt()
+    if (isU512(n)) return new CairoUint512(n).toBigInt()
+    throw new Error(`Invalid cairoInt ${JSON.stringify(n)}`)
   }
   return num.toBigInt(n)
 }
