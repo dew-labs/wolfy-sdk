b7271fd9f8580fa7aca46ee581a2c7ce0e24c279
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2024-12-02 16:41:08 +0700

    🐛 fix(app): types

--- src/multicall.ts
+++ src/multicall.ts
@@ -1,75 +1,48 @@
-import type {
-  Abi,
-  ExtractAbiFunctionNames,
-  FunctionArgs,
-  FunctionRet,
-} from 'node_modules/abi-wan-kanabi/dist/kanabi'
-import {CallData, hash} from 'starknet'
-import {createMulticallRequest} from 'starknet_multicall'
-import invariant from 'tiny-invariant'
+import type {ExtractAbiFunctionNames} from 'node_modules/abi-wan-kanabi/dist/kanabi'
+import type {AccountInterface} from 'starknet'
+import {
+  type CallAndAbi,
+  type ContractMethodArgs,
+  createMulticallRequest,
+  multicall,
+  type MulticallResult,
+} from 'starknet_multicall'
 
-import MulticallABI from './abis/MulticallABI'
-import type {StarknetChainId} from './chains'
-import {createWolfyContract, WolfyContract, type WolfyContractAbi} from './contracts'
+import {StarknetChainId} from './chains'
+import {getWolfyContractAddress, WolfyContract, type WolfyContractAbi} from './contracts'
+import {getProvider, ProviderType} from './rpcProviders'
 
 export function createWolfyMulticallRequest<
   ContractName extends WolfyContract,
   ContractAbi extends WolfyContractAbi<ContractName>,
   Method extends ExtractAbiFunctionNames<ContractAbi>,
-  // @ts-expect-error -- complex typescript
-  Args extends FunctionArgs<ContractAbi, Method> extends unknown[] // note: we have to do this because some how FunctionArgs is wrong for single element
-    ? // @ts-expect-error -- complex typescript
-      FunctionArgs<ContractAbi, Method>
-    : // @ts-expect-error -- complex typescript
-      [FunctionArgs<ContractAbi, Method>],
 >(
   chainId: StarknetChainId,
   contractName: ContractName,
   abi: ContractAbi,
   method: Method,
-  args?: Args,
+  // @ts-expect-error -- complex typescript
+  args?: ContractMethodArgs<ContractAbi, Method>,
 ) {
-  const contract = createWolfyContract(chainId, contractName, abi)
   return createMulticallRequest(
-    contract.address,
+    getWolfyContractAddress(chainId, contractName),
     abi,
     method as ExtractAbiFunctionNames<ContractAbi>,
     args,
-  )
+  ) as {
+    abi: ContractAbi
+    contractAddress: string
+    entrypoint: Method
+    calldata: bigint[]
+  }
 }
 
-interface CallAndAbi {
-  abi: Abi
-  contractAddress: string
-  entrypoint: string
-  calldata: bigint[] | [bigint]
-}
-
-export async function wolfyMulticall<T extends CallAndAbi, Ts extends T[]>(
+export async function wolfyMulticall<T extends CallAndAbi, Ts extends readonly T[]>(
   chainId: StarknetChainId,
   calls: Ts,
-): Promise<{[k in keyof Ts]: FunctionRet<Ts[k]['abi'], Ts[k]['entrypoint']>}> {
-  const multicallContract = createWolfyContract(chainId, WolfyContract.Multicall, MulticallABI)
-
-  const results = await multicallContract.aggregate(
-    calls.map(call => {
-      return {
-        to: call.contractAddress,
-        selector: hash.getSelectorFromName(call.entrypoint),
-        calldata: call.calldata,
-      }
-    }),
-  )
-
-  const result = results[1]
-
-  invariant(Array.isArray(result), 'multicall returned an invalid result')
-
-  return result.map((result, index) => {
-    const call = calls[index]
-    if (!call)
-      throw new Error('call is undefined, something went wrong with the multicall aggregation')
-
-    return new CallData(call.abi).parse(call.entrypoint, result as string[])
-  }) as {[k in keyof Ts]: FunctionRet<Ts[k]['abi'], Ts[k]['entrypoint']>}
+  account?: AccountInterface,
+): MulticallResult<T, Ts> {
+  const multicallContractAddress = getWolfyContractAddress(chainId, WolfyContract.Multicall)
+  const provider = account ?? getProvider(ProviderType.HTTP, chainId)
+  return await multicall(calls, multicallContractAddress, provider)
 }
